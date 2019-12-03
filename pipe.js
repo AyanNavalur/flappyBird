78efd1d61b60e3a8cab0938676a1098e1c40c3ec
Ayan <ayan.navalur@gmail.com>
2019-12-03 19:14:52 +0530

    'v1.0'

---
+++ pipe.js
@@ -0,0 +1,50 @@
+function Pipe() {
+  this.top = random(height-100);
+  this.bottom = this.top + 100;
+  //this.bottom = random(height/2);
+  this.x = width;
+  this.w = 20;
+  this.speed = 1;
+
+  this.highlight = false;
+
+  this.hits = function(bird) {
+    if (bird.x >this.x && bird.x < this.x + this.w) {
+      if(bird.y < this.top || bird.y > this.bottom) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  }
+
+  this.calcScore = function() {
+    if (bird.x === (this.x + this.w)) {
+      //console.log(bird.x, this.x + this.w);
+      return true;
+    }
+    return false;
+  }
+
+  this.show = function() {
+    fill(255);
+    if(this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, this.bottom, this.w, height);
+  }
+
+  this.update = function() {
+    this.x -=this.speed
+  }
+
+  this.offscreen = function() {
+    if (this.x<-this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,77 @@
+var bird;
+var pipes = [];
+//var k=0;
+
+var score = 0;
+let scoreP;
+
+function setup() {
+	createCanvas(400, 600);
+	scoreP = createP('Score: 0');
+	scoreP.style('font-size', '20pt');
+	//scoreP.position(10,10);
+
+	bird = new Bird();
+	pipes.push(new Pipe());
+}
+
+function draw() {
+	background(0);
+
+	for (var i = pipes.length-1 ; i >=0 ; i--) {
+		pipes[i].show();
+		pipes[i].update();
+		if(pipes[i].calcScore(bird)) {
+			score++;
+		}
+		scoreP.html('Score: '+`${score}`);
+
+		if(pipes[i].hits(bird)) {
+			console.log('HIT');
+			pipes[i].show();
+			//console.log('Here');
+			//noLoop();
+			reset();
+			//k = 1;
+		}
+		if (pipes.length !== 0) {
+			if (pipes[i].offscreen()) {
+				pipes.splice(i, 1);
+				//console.log(pipes.length);
+			}
+		}
+	}
+
+	bird.show();
+	bird.update();
+
+
+	if(frameCount % 200 == 0) {
+		pipes.push(new Pipe());
+	}
+
+}
+
+function reset() {
+	pipes = [];
+	bird.y = height/2;
+  bird.x = 25;
+	bird.velocity = 0;
+	score = 0;
+
+}
+
+function keyPressed() {
+	if (key == ' ') {
+		bird.up();
+	}
+	//code for waiting until you press the spacebar after you die to resume gameplay...
+	//ISSUE: page is unresponsive if you leave it for more than 20 seconds on loop
+	// while(k) {
+	// 	if (key == ' ') {
+	// 		reset();
+	// 		loop();
+	// 		k = 0;
+	// 	}
+	// }
+}
